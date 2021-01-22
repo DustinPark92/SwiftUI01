11d2f678c17bbdd3074ab868fe0d59c76f4ffb6c
DustinPark92 <p4569zz@gmail.com>
2021-01-22 16:54:38 +0900

    2020.01.22 @state , @binding

--- SwiftUI01/ContentView.swift
+++ SwiftUI01/ContentView.swift
@@ -8,18 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // @State 값의 변화를 감지 -> 뷰에 적용
+    @State
+    private var isActivated: Bool = false
+    
+    // 몸체
     var body: some View {
-
         
-        HStack {
-            MyVstackView()
-            MyVstackView()
-            MyVstackView()
-            MyVstackView()
-
-        }
-        .padding(.all) // HSstack
-        .background(Color.yellow)
+        NavigationView{
+            VStack{
+                HStack{
+                    MyVstackView(isActivated: $isActivated)
+                    MyVstackView(isActivated: $isActivated)
+                    MyVstackView(isActivated: $isActivated)
+                    
+                } // Hstack
+                    .padding(isActivated ? 50.0 : 10.0)
+                    //
+                    .background(isActivated ? Color.yellow : Color.white)
+                   // 탭 재스쳐 추가
+                    .onTapGesture {
+                        print("HStack 이 클릭되었다.")
+                        
+                        // 애니메이션과 함께
+                        withAnimation {
+                            // toggle() true 이면 false 로 false 이면 true
+                            self.isActivated.toggle()
+                        }
+                }// Hstack
+                
+                // 네비게이션 버튼(링크)
+                NavigationLink(destination: MyTextView(isActivated: $isActivated) ){
+                    Text("네비게이션")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(30)
+                    
+                } .padding(.top, 50)
+            }
+            
+        } // NavigationView
+        
+        
+        
     }
 }
 
--- SwiftUI01/MyVstackView.swift
+++ SwiftUI01/MyVstackView.swift
@@ -7,9 +7,32 @@
 
 import SwiftUI
 
-struct MyVstackView: View {
+struct MyVstackView : View {
+    
+    // 데이터를 연동 시킨다
+    @Binding
+    var isActivated : Bool
+    
+    // 생성자
+    init(isActivated : Binding<Bool> = .constant(false)) {
+        _isActivated = isActivated
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("1")
+                .padding()
+                .font(.system(size: 30))
+            Text("2")
+                .padding()
+                .font(.system(size: 30))
+            Text("3")
+                .padding()
+                .font(.system(size: 30))
+        } // VStack
+        .background(self.isActivated ? Color.green : Color.red)
+        .padding(self.isActivated ? 10 : 0)
     }
 }
 
