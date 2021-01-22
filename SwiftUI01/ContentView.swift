471b076ea639dee949a99278b58bd0c39a8b9f07
DustinPark92 <p4569zz@gmail.com>
2021-01-22 16:00:21 +0900

    #1

--- SwiftUI01/ContentView.swift
+++ SwiftUI01/ContentView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        
+        HStack {
+            MyVstackView()
+            MyVstackView()
+            MyVstackView()
+            MyVstackView()
+
+        }
+        .padding(.all) // HSstack
+        .background(Color.yellow)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SwiftUI01/MyVstackView.swift
@@ -0,0 +1,20 @@
+//
+//  MyVstackView.swift
+//  SwiftUI01
+//
+//  Created by Dustin on 2021/01/22.
+//
+
+import SwiftUI
+
+struct MyVstackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyVstackView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyVstackView()
+    }
+}
